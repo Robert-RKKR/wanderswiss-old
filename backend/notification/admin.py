351ef04bd5debf122051b6d730298593437c879e
robertrkkr <robert.kucharski.rkkr@gmail.com>
2024-10-13 20:48:27 +0200

    bag fix

--- backend/notification/admin.py
+++ backend/notification/admin.py
@@ -7,58 +7,9 @@ from wanderswiss.base.admins.based_admin import BaseAdmin
 # WanderSwiss - notifications model import:
 from notification.models.notification_model import NotificationModel
 from notification.models.change_log_model import ChangeLogModel
-from notification.models.log_model import LogModel
 
 
 # All notification admin classes:
-@admin.register(LogModel)
-class LogAdmin(BaseAdmin):
-
-    list_display = (
-        'pk', 'timestamp', 'application', 'object_representation', 'severity',
-        'task_id', 'message',
-    )
-    list_display_links = (
-        'pk',
-    )
-    list_filter = (
-        'app_name', 'model_name', 'severity',
-    )
-    search_fields = (
-        'message', 'object_representation', 'task_id', 'object_id',
-    )
-    fieldsets = (
-        ('Basic information', {
-            'classes': ('wide', 'extrapretty',),
-            'fields': ('severity', 'task_id', 'application',)
-        }),
-        ('Related object information', {
-            'classes': ('wide', 'extrapretty',),
-            'fields': ('app_name', 'model_name',
-                'object_representation', 'object_id'),
-        }),
-        ('Message', {
-            'classes': ('wide', 'extrapretty',),
-            'fields': ('message',),
-        }),
-        ('Execution time', {
-            'classes': ('wide', 'extrapretty',),
-            'fields': ('execution_time',),
-        }),
-        ('Additional data', {
-            'classes': ('wide', 'extrapretty',),
-            'fields': ('additional_data',),
-        }),
-    )
-    readonly_fields = (
-        'timestamp', 'object_representation', 'severity',
-        'task_id', 'message', 'app_name', 'execution_time',
-        'model_name', 'object_representation', 'object_id',
-        'additional_data', 'application',
-    )
-    empty_value_display = '--None--'
-
-
 @admin.register(NotificationModel)
 class NotificationAdmin(BaseAdmin):
 
--- backend/notification/models/__init__.py
+++ backend/notification/models/__init__.py
@@ -1,5 +1,3 @@
 # Application import:
-from .object_representation_model import ObjectRepresentationModel
 from .notification_model import NotificationModel
 from .change_log_model import ChangeLogModel
-from .log_model import LogModel
